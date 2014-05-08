5b8167403b447ed8337e4d6a5b5d14667141cef5
tjy86 <thomasjyang@gmail.com>
2014-05-08 18:22:56 -0400

    pay done

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,9 +1,9 @@
 # Stripe.api_key = ""
 # STRIPE_PUBLIC_KEY = ""
 Rails.configuration.stripe = {
-  :publishable_key => "pk_test_fJUnhBBBk3cRl1vUUsEIRrUU"
+  :publishable_key => "pk_live_gztqxkPZkzDVwZfXZZptBYio"
   # :secret_key      => sk_test_IsYVoEHQpf62rvxb8HG1PM6a
 }
 
 # Stripe.api_key = Rails.configuration.stripe[:secret_key]
-Stripe.api_key = "sk_test_IsYVoEHQpf62rvxb8HG1PM6a"
+Stripe.api_key = ENV["STRIPE_SECRET"]
